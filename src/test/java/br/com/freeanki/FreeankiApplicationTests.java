85ed88af5af9794b5c5a9be12a3b1ff9e97399df
Thiago Santana <thiago91098@gmail.com>
2021-03-14 19:52:52 -0300

    Initial commit

---
+++ src/test/java/br/com/freeanki/FreeankiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.freeanki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeankiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

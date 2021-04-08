22bef02c1145526e5a9dd68cf3c9838c69d9b499
Welbert Fernandes Costa <welbert.jsj@gmail.com>
2021-04-08 15:34:36 -0300

    create constructor and anotaction

--- src/main/java/br/com/freeanki/models/Card.java
+++ src/main/java/br/com/freeanki/models/Card.java
@@ -1,9 +1,18 @@
 package br.com.freeanki.models;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import java.util.Date;
 
+@Data
 public class Card {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
     private String deckId;
     private String question;
     private String answer;
@@ -11,4 +20,8 @@ public class Card {
     private Integer lastUserAnswer;
     private Integer answerAmount;
     private Date dueDate;
+
+    private Card(){
+
+    }
 }
--- src/main/java/br/com/freeanki/models/Deck.java
+++ src/main/java/br/com/freeanki/models/Deck.java
@@ -1,13 +1,27 @@
 package br.com.freeanki.models;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.io.Serializable;
 import java.util.List;
 
-public class Deck {
+@Data
+public class Deck implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
     private String name;
     private String description;
     private String tags;
     private String category;
     private List<Card> cards;
+
+    private Deck(){
+
+    }
 }

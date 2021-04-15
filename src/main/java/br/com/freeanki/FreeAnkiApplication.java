3dc5943356911356fe7890a10d2ce7a2a6770aeb
NewtonCesarRoncari <new123cesar@gmail.com>
2021-04-14 23:45:31 -0300

    fixed viewed

--- src/main/java/br/com/freeanki/FreeAnkiApplication.java
+++ src/main/java/br/com/freeanki/FreeAnkiApplication.java
@@ -3,7 +3,7 @@ package br.com.freeanki;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication()
+@SpringBootApplication
 public class FreeAnkiApplication {
 
     public static void main(String[] args) {
